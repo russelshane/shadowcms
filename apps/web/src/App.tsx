dea34b90e025e745e065acb7f54831bd117d084b
russelshane <darsler@pm.me>
2021-10-10 21:25:25 +0800

    (stg): setup new shadowcms monorepo

---
+++ apps/web/src/App.tsx
@@ -0,0 +1,14 @@
+/**
+ * Shadow C.M.S. Client Application
+ * Copyright (c) 2022
+ *
+ * @author ShadowCMS
+ */
+
+import React from "react";
+
+const App: React.FC = () => {
+  return <div className="App"></div>;
+};
+
+export default App;
---
+++ apps/web/src/index.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import App from "./App";
+import { render } from "react-dom";
+
+render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById("react-root"),
+);
