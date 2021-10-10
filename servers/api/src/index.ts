1c579f20541d8377654ef30624d0c0a6be5404f9
russelshane <darsler@pm.me>
2021-10-10 21:42:19 +0800

    (stg): new API server infrastructure

---
+++ servers/api/src/index.ts
@@ -0,0 +1,6 @@
+/**
+ * This is the main code for the Shadow CMS' API.
+ *  Copyright (c) 2022
+ *
+ * @author ShadowCMS
+ */
---
+++ servers/api/src/utilities/logger.ts
@@ -0,0 +1,16 @@
+import memoize from "micro-memoize";
+import pino from "pino";
+
+export type ShadowLogger = pino.Logger;
+
+export default memoize(
+  (name = "shadow") =>
+    pino({
+      name,
+      enabled: true,
+      prettyPrint: {
+        ignore: "pid, hostname",
+        translateTime: "HH:MM:ss",
+      },
+    }) as ShadowLogger,
+);
