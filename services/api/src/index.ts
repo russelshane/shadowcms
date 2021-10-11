594cb42e4256e252859b24829853f0c761d3f69d
russelshane <darsler@pm.me>
2021-10-11 21:57:03 +0800

    (stg): v2.4.1 rest and graphql api for ShadowCMS

---
+++ services/api/src/index.ts
@@ -0,0 +1,9 @@
+/**
+ * Main REST and GraphQL API Service for Shadow CMS
+ *
+ * @author ShadowCMS
+ */
+
+import logger from './utilities/logger';
+
+logger.info('Starting Shadow CMS API Service Dope...');
