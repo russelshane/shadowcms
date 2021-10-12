7fb5e56ace8678347b3d87d5c67eaee831374e00
russelshane <darsler@pm.me>
2021-10-12 16:03:16 +0800

    (stg): rename api-staging to just api

---
+++ services/api/src/index.ts
@@ -0,0 +1,10 @@
+/**
+ * Main codebase for the API Service. This code may be
+ * pushed for production.
+ *
+ * @author ShadowCMS
+ */
+
+import logger from './utilities/logger';
+
+logger.info('Starting Shadow CMS API Service Dope...');
